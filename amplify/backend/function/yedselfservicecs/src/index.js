1d42f28bd25297c46b6280c5d123354eb7e61d8e
csalas <c.salas@yubico.com>
2021-10-06 12:59:31 -0500

    changes made to amplify function

--- amplify/backend/function/yedselfservicecs/src/index.js
+++ amplify/backend/function/yedselfservicecs/src/index.js
@@ -1,12 +1,21 @@
-const AWS = require("aws-sdk");
+const aws = require("aws-sdk");
 
 /**
- * TODO
- * Function to make a request to the AWS Secrets Manager to pull in the YED secret for operations
- * @param - N/A
- * @returns API Secret for the YED API
+ * Amplify generated way to receive tokens
  */
-function getSecret() {};
+const { Parameters } = await new aws.SSM()
+  .getParameters({
+    Names: ["yedtoken", "yedcookie"].map(
+      (secretName) => process.env[secretName]
+    ),
+    WithDecryption: true,
+  })
+  .promise();
+
+/**
+ * Set ENV variable for your YED API URL
+ */
+const yed_url = process.env.yedurl;
 
 /**
  * TODO
@@ -14,7 +23,7 @@ function getSecret() {};
  * @param - token that was received when making the request
  * @returns - Object with user details pulled from the token
  */
- function validateJWT() {};
+function validateJWT() {}
 
 /**
  * TODO
@@ -23,7 +32,7 @@ function getSecret() {};
  * @param - N/A
  * @returns - List of objects containing specific information about the keys
  */
-function getInventoryKeys(){};
+function getInventoryKeys() {}
 
 /**
  * TODO
@@ -32,67 +41,94 @@ function getInventoryKeys(){};
  * @param, - Address entered by the user
  * @returns - True if the address is valid, false otherwise (this may be extended to include recommendations in a later release)
  */
-function verifyAddress(userAddress){};
+function verifyAddress(userAddress) {}
 
 /**
  * TODO
  * Function to return detailed information about a specific order
  * @param - ID of the oder
- * @returns - Object containing details about an order 
+ * @returns - Object containing details about an order
  */
-function getOrder(orderID){};
+function getOrder(orderID) {}
 
 /**
  * Will return a collection of orders (if the user is able to have multiple orders) - Otherwise it will be a one item collection
  * @param - ID of the user
  * @returns - Collection of orders belonging to the user
  */
-function getOrders(userID){};
+function getOrders(userID) {}
 
 /**
  * Allows a user to create a new order
  * @param - Details of the shipment to POST to YED
  * @returns - Standard YED response object with order details
  */
- function createOrder(shipmentDetails){}
+function createOrder(shipmentDetails) {}
 
 /**
  * Allows a user to edit a specific order
  * @param - ID of the order to change, and the details needed to update the order
  * @returns - Standard YED response object with order details
  */
-function editOrder(orderID, shipmentChanges){}
+function editOrder(orderID, shipmentChanges) {}
 
 /**
  * Allows a user to delete a specific order
  * @param - ID of the order to delete
  * @returns - Confirmation message that the item was deleted
  */
- function deleteOrder(orderID){}
+function deleteOrder(orderID) {}
 
 exports.handler = async (event) => {
-    // TODO implement
-    let body;
-    let statusCode = 200;
-    const headers = {
-        "Content-Type": "application/json"
-    };
+  // TODO implement
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
+  };
 
-
-    try {
-        switch (event.routeKey) {
-
-        }
-    } catch (err) {
-        statusCode = 400;
-        body = err.message;
-    } finally {
-        body = JSON.stringify(body);
+  /*
+  try {
+    switch (event.routeKey) {
+      case "GET /yed/inventory":
+            body = { "Message": "In the GET inventory method" }
+        break;
+      case "POST /yed/address":
+        body = { "Message": "In the POST address" }
+        break;
+      case "GET /yed/order/{id}":
+        body = { "Message": `In the GET order method with param  "${event.pathParameters.id}"`  }
+        break;
+      case "GET /yed/orders/{id}":
+        body = { "Message": `In the GET orders method with param  "${event.pathParameters.id}"`  }
+        break;
+      case "POST /yed/order":
+        body = { "Message": `In the POST order method`  }
+        break;
+      case "PUT /yed/order/{id}":
+        body = { "Message": `In the PUT order method with param  "${event.pathParameters.id}"`  }
+        break;
+      case "DELETE /yed/order/{id}":
+        body = { "Message": `In the DELETE orders method with param  "${event.pathParameters.id}"`  }
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
     }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+  */
 
-    return {
-        statusCode,
-        body,
-        headers
-    };
+  body = {"message": "Hello World"}
+
+  body = JSON.stringify(body);
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 };
